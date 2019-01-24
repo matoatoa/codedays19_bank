ad672a4aeee093770d931e918811436e16d1003e
Jan Hauer <Jan.Hauer@EXXETA.com>
2019-01-24 11:45:07 +0100

    Schritt 05: Vergleich Threads

--- client/src/main/java/de/matoatoa/demo/codedays19/bank/client/Controller.java
+++ client/src/main/java/de/matoatoa/demo/codedays19/bank/client/Controller.java
@@ -1,16 +1,21 @@
 package de.matoatoa.demo.codedays19.bank.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @RestController
 public class Controller {
+    private final static Logger LOGGER = LoggerFactory.getLogger(Controller.class);
+    private final static Consumer<MarketData> LOG_RESULT = datum -> LOGGER.info("Got result:{}", datum);
 
     private final static List<String> COMPANIES = List.of("Google", "Amazon", "BMV", "Microsoft", "Apple", "SAP", "Audi", "Daimler", "Telekom", "Bosch", "Siemens", "IBM", "Porsche", "VW", "DB");
     private final RestTemplate restTemplate;
@@ -23,6 +28,6 @@ public class Controller {
     public List<MarketData> getMarketData() {
         final Function<String, MarketData> CALL_SERVER =
                 id -> restTemplate.getForObject("/api/market/" + id, MarketData.class, Collections.emptyMap());
-        return COMPANIES.stream().parallel().map(CALL_SERVER).collect(Collectors.toList());
+        return COMPANIES.stream().parallel().map(CALL_SERVER).peek(LOG_RESULT).collect(Collectors.toList());
     }
 }
