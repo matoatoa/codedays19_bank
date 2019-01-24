399626123d990b03016bbe45bc41e18f2dfc6da6
Jan Hauer <Jan.Hauer@EXXETA.com>
2019-01-24 11:18:35 +0100

    Schritt 02: Client-Modul + Server-Port

---
+++ client/src/main/java/de/matoatoa/demo/codedays19/bank/client/ClientApplication.java
@@ -0,0 +1,16 @@
+package de.matoatoa.demo.codedays19.bank.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+}
+
